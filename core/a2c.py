c50445a8b27de265d73769c86b3e2c23246de144
Ren <s.ren@tue.nl>
2023-05-16 17:55:02 +0200

    script for RL

---
+++ core/a2c.py
@@ -0,0 +1,43 @@
+import torch
+
+
+def a2c_step(policy_net, value_net, optimizer_policy, optimizer_value, states, actions, returns, advantages, l2_reg):
+
+    """update critic"""
+    values_pred = value_net(states)
+    value_loss = (values_pred - returns).pow(2).mean()
+    # weight decay
+    for param in value_net.parameters():
+        value_loss += param.pow(2).sum() * l2_reg
+    optimizer_value.zero_grad()
+    value_loss.backward()
+    optimizer_value.step()
+
+    """update policy"""
+    log_probs = policy_net.get_log_prob(states, actions)
+    policy_loss = -(log_probs * advantages).mean()
+    optimizer_policy.zero_grad()
+    policy_loss.backward()
+    torch.nn.utils.clip_grad_norm_(policy_net.parameters(), 40)
+    optimizer_policy.step()
+
+def assembly_a2c_step(ac_net, optimizer, values, states, actions, returns, advantages, l2_reg):
+
+    """update critic"""
+    values_pred = values
+    value_loss = (values_pred - returns).pow(2).mean()
+
+    # # weight decay
+    # for param in value_net.parameters():
+    #     value_loss += param.pow(2).sum() * l2_reg
+
+    """update policy"""
+    log_probs = ac_net.get_log_prob(states, actions)
+    policy_loss = -(log_probs * advantages).mean()
+
+    total_loss = value_loss + policy_loss
+
+    optimizer.zero_grad()
+    total_loss.backward()
+    torch.nn.utils.clip_grad_norm_(ac_net.parameters(), 40)
+    optimizer.step()
---
+++ models/cnn_ac.py
@@ -0,0 +1,72 @@
+import torch.nn as nn
+import torch
+from utils.math import *
+
+
+feature_dim = 32
+
+class BinaryCnnAC(nn.Module):
+    def __init__(self, action_dim, activation='sigmoid', log_std=0):
+        super().__init__()
+
+        self.is_disc_action = False
+        if activation == 'tanh':
+            self.activation = torch.tanh
+        elif activation == 'relu':
+            self.activation = torch.relu
+        elif activation == 'sigmoid':
+            self.activation = torch.sigmoid
+
+        self.cnn = nn.Sequential(
+
+            nn.Tanh(),
+            nn.MaxPool2d(10, stride=10),
+
+            nn.Conv2d(1, 8, kernel_size=7, stride=3),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+
+            nn.Conv2d(8, 2, kernel_size=3, stride=1),
+
+        )
+
+        self.linear = nn.Sequential(
+            nn.Linear(2*14*14, feature_dim), 
+            nn.Tanh()
+            )
+        
+        self.value_head = nn.Linear(feature_dim, 1)
+        self.value_head.weight.data.mul_(0.1)
+        self.value_head.bias.data.mul_(0.0)
+
+        self.action_mean = nn.Linear(feature_dim, action_dim)
+        self.action_mean.weight.data.mul_(0.1)
+        self.action_mean.bias.data.mul_(0.0)
+
+        self.action_log_std = nn.Parameter(torch.ones(1, action_dim) * log_std)
+
+    def forward(self, arr_img):
+
+        x = self.cnn(arr_img)
+        x = x.view(-1, 2*14*14)
+        x = self.linear(x)
+
+        action_mean = self.action_mean(x)
+        action_log_std = self.action_log_std.expand_as(action_mean)
+        action_std = torch.exp(action_log_std)
+
+        value = self.value_head(x)
+
+        return value, action_mean, action_log_std, action_std
+
+    def select_action(self, x):
+        _, action_mean, _, action_std = self.forward(x)
+        action = torch.normal(action_mean, action_std)
+        return action
+
+    def get_log_prob(self, x, actions):
+        _, action_mean, action_log_std, action_std = self.forward(x)
+        return normal_log_density(actions, action_mean, action_log_std, action_std)
+
+
+

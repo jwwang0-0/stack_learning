3f6dfadc18511b5e9b206c9a2b8420e06e64af12
Jingwen <wang@dbt-ethz.ch>
2023-05-23 20:48:39 +0200

    Update random seed and save model hyperparameter

--- examples/a2c_gym2.py
+++ examples/a2c_gym2.py
@@ -35,9 +35,9 @@ parser.add_argument('--l2-reg', type=float, default=1e-3, metavar='G',
                     help='l2 regularization regression (default: 1e-3)')
 parser.add_argument('--num-threads', type=int, default=1, metavar='N',
                     help='number of threads for agent (default: 4)')
-parser.add_argument('--seed', type=int, default=1, metavar='N',
+parser.add_argument('--seed', type=int, default=333, metavar='N',
                     help='random seed (default: 1)')
-parser.add_argument('--min-batch-size', type=int, default=128, metavar='N',
+parser.add_argument('--min-batch-size', type=int, default=512, metavar='N',
                     help='minimal batch size per A2C update (default: 2048)')
 parser.add_argument('--eval-batch-size', type=int, default=32, metavar='N',
                     help='minimal batch size for evaluation (default: 2048)')
@@ -45,7 +45,7 @@ parser.add_argument('--max-iter-num', type=int, default=5000, metavar='N',
                     help='maximal number of main iterations (default: 500)')
 parser.add_argument('--log-interval', type=int, default=1, metavar='N',
                     help='interval between training status logs (default: 1)')
-parser.add_argument('--save-model-interval', type=int, default=250, metavar='N',
+parser.add_argument('--save-model-interval', type=int, default=100, metavar='N',
                     help="interval between saving model (default: 0, means don't save)")
 parser.add_argument('--gpu-index', type=int, default=0, metavar='N')
 args = parser.parse_args()

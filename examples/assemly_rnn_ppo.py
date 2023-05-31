cbb2e3c1026d0a37bfc81e11aa8dd1ef0b4d5835
Ren <s.ren@tue.nl>
2023-05-31 19:21:11 +0200

    make plots

--- examples/assemly_rnn_ppo.py
+++ examples/assemly_rnn_ppo.py
@@ -32,11 +32,11 @@ max_iter_num = 2000
 log_interval = 1
 
 parser = argparse.ArgumentParser(description='PyTorch A2C example')
-parser.add_argument('--log-std', type=float, default=-1, metavar='G',
+parser.add_argument('--log-std', type=float, default=-1.5, metavar='G',
                     help='log std for the policy (default: -0.0)')
-parser.add_argument('--hidden-n', type=int, default=64, metavar='G',
+parser.add_argument('--hidden-n', type=int, default=8, metavar='G',
                     help='number of hidden neurons in RNN (default: 64)')
-parser.add_argument('--hidden-l', type=int, default=2, metavar='G',
+parser.add_argument('--hidden-l', type=int, default=1, metavar='G',
                     help='number of hidden layers in RNN (default: 2)')
 parser.add_argument('--gamma', type=float, default=1, metavar='G',
                     help='discount factor (default: 0.99)')
@@ -52,6 +52,8 @@ parser.add_argument('--save-model-interval', type=int, default=0, metavar='N',
                     help="interval between saving model (default: 0, means don't save)")
 parser.add_argument('--clip-epsilon', type=float, default=0.2, metavar='N',
                     help='clipping epsilon for PPO')
+parser.add_argument('--seed', type=int, default=1234, metavar='N',
+                    help='random seed')
 args = parser.parse_args()
 
 dtype = torch.float32
@@ -67,10 +69,10 @@ running_state = None
 # running_reward = ZFilter((1,), demean=False, clip=10)
 
 """seeding"""
-# seed = 2345
-# np.random.seed(seed)
-# torch.manual_seed(seed)
-# env.seed(seed)
+seed = args.seed #[28,7,384,93,571,3947,39,102]
+np.random.seed(seed)
+torch.manual_seed(seed)
+env.seed(seed)
 
 """define actor and critic"""
 # combine the policy and value net into one
@@ -147,33 +149,32 @@ def main():
         update_params(batch)
         t1 = time.time()
 
+        with open(LOG_PATH+'log.txt', 'a') as f:
+            with redirect_stdout(f):
+                print(''.join(['=']*40) + ' Evaluation '+ ''.join(['=']*40))
+        """evaluate with determinstic action (remove noise for exploration)"""
+        _, log_eval = agent.collect_samples(eval_batch_size, mean_action=True)
+        t2 = time.time()
+
+        print('{}\tT_sample {:.4f}\tT_update {:.4f}\tT_eval {:.4f}\ttrain_R_min {:.2f}\ttrain_R_max {:.2f}\ttrain_R_avg {:.2f}\teval_R_avg {:.2f}'.format(
+                i_iter, log['sample_time'], t1-t0, t2-t1, log['min_reward'], log['max_reward'], log['avg_reward'], log_eval['avg_reward']))
+
         hist_reward.append({'avg': log.get('avg_reward'),
                             'min': log.get('min_reward'),
                             'max': log.get('max_reward'),
+                            'eval_avg': log_eval['avg_reward']
                             })
-        hist_maxheight.append(log.get('max_height'))
-        hist_pos.append(log.get('hist_pos'))
+        # hist_maxheight.append(log.get('max_height'))
+        # hist_pos.append(log.get('hist_pos'))
 
-        if i_iter % log_interval == 0:
+        if i_iter % 10 == 0:
+            pd.DataFrame.from_records(hist_reward).to_csv(DATA_PATH + 'reward_{}_{}.csv'.format(seed, i_iter))
             # pd.DataFrame.from_records(hist_reward).to_csv(DATA_PATH + 'reward.csv', mode='a')
             # pd.Series(hist_maxheight).to_csv(DATA_PATH + 'height.csv', mode='a')
             # pd.Series(hist_pos).to_csv(DATA_PATH + 'pose.csv', mode='a')
             # hist_reward = []
             # hist_maxheight = []
             # hist_pos = []
-
-            with open(LOG_PATH+'log.txt', 'a') as f:
-                with redirect_stdout(f):
-                    print(''.join(['=']*40) + ' Evaluation '+ ''.join(['=']*40))
-            """evaluate with determinstic action (remove noise for exploration)"""
-            _, log_eval = agent.collect_samples(eval_batch_size, mean_action=True)
-            t2 = time.time()
-
-            print('{}\tT_sample {:.4f}\tT_update {:.4f}\tT_eval {:.4f}\ttrain_R_min {:.2f}\ttrain_R_max {:.2f}\ttrain_R_avg {:.2f}\teval_R_avg {:.2f}'.format(
-                i_iter, log['sample_time'], t1-t0, t2-t1, log['min_reward'], log['max_reward'], log['avg_reward'], log_eval['avg_reward']))
-
-        if i_iter >= 30:
-            breakpoint()
 
         # if save_model_interval > 0 and (i_iter+1) % save_model_interval == 0:
         #     to_device(torch.device('cpu'), policy_net, value_net)
